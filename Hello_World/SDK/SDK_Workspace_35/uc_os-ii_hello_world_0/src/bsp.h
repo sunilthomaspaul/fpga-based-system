b8a776aa285f8c46ecc93b3cefcbf60e1066cf6d
hisho218 <hisho218@4597a959-786a-7d49-a10f-c50fb64b552e>
2010-11-19 01:50:03 +0000

    Adding the sane hello program to the repository

---
+++ Hello_World/SDK/SDK_Workspace_35/uc_os-ii_hello_world_0/src/bsp.h
@@ -0,0 +1,45 @@
+/*
+*********************************************************************************************************
+*                                             uC/OS-II
+*                                        The Real-Time Kernel
+*
+*                          (c) Copyright 1992-2010, Micrium, Inc., Weston, FL
+*                                        All Rights Reserved
+* 
+* File        : bsp.h
+* Programmer  : AHFAI
+*               NB
+*********************************************************************************************************
+*/
+
+#ifndef __BSP_H__
+#define __BSP_H__
+
+#include   "includes.h"
+
+/*
+*********************************************************************************************************
+*                                             CONSTANTS
+*********************************************************************************************************
+*/
+
+#define  BSP_INTC_DEVICE_ID     XPAR_XPS_INTC_0_DEVICE_ID
+#define  BSP_INTC_TIMER1_ID     XPAR_INTC_0_TMRCTR_0_VEC_ID
+#define  BSP_INTC_ADDR          XPAR_INTC_0_BASEADDR
+#define  BSP_TIMER0_ADDR        XPAR_XPS_TIMER_0_BASEADDR
+
+#define  BSP_TMR_VAL            (XPAR_CPU_DPLB_FREQ_HZ / OS_TICKS_PER_SEC)
+
+/*
+*********************************************************************************************************
+*                                             PROTOTYPES
+*********************************************************************************************************
+*/
+
+void  BSP_TmrInit            (void);
+void  BSP_Init               (void);
+void  BSP_IntDisAll          (void);
+void  BSP_InitIntCtrl        (void);
+void  BSP_InitIO             (void);
+
+#endif
